d1e7315959eb20e3ee4878c83efbc7c87c2b6377
Chu A. <chu.amk22@gmail.com>
2025-12-17 14:45:24 +0100

    feat(ui): Implement Header component with styling

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Header.css";
+import { CgProfile } from "react-icons/cg";
+import { FaUserCircle } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1 className="title">
+        Xchange <span>Manage and exchange your multi-currency portfolio</span>
+      </h1>
+      <div className="profile">
+        <FaUserCircle className="profile-icon" />
+        <span className="user-name">username</span>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
