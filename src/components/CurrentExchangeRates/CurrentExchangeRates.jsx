6842bdc02124c0ea64c9e48d27cb686ee9e94178
Chu A. <chu.amk22@gmail.com>
2025-12-22 12:28:06 +0100

    feat(ui): Implement Exchange  rates component

---
+++ src/components/CurrentExchangeRates/CurrentExchangeRates.jsx
@@ -0,0 +1,22 @@
+import "./CurrentExchangeRates.css";
+
+const CurrentExchangeRates = () => {
+  return (
+    <div className="current-exchange-rate">
+      <h3>Current Exchange Rates</h3>
+      <div className="container">
+        <div className="rates">
+          <p>1 USD = 0.9524 EUR</p> <p>1 EUR = 656.25 XAF</p>
+        </div>
+        <div className="rates">
+          <p>1 USD = 0.9524 EUR</p> <p>1 EUR = 656.25 XAF</p>
+        </div>
+        <div className="rates">
+          <p>1 USD = 0.9524 EUR</p> <p>1 EUR = 656.25 XAF</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CurrentExchangeRates;
